16c4f4f00cdf8d9b69cd81f0df3bb523dca8199d
孔大夫 <25993391+kongdf@users.noreply.github.com>
2024-11-22 15:33:47 +0800

    [删除大盘价格]

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,71 @@
 <script setup>
-import { reactive } from 'vue';
-import { fetch } from '@tauri-apps/plugin-http';
-import WebSocket from '@tauri-apps/plugin-websocket';
-import { getCurrentWindow } from '@tauri-apps/api/window';
+import { reactive } from "vue";
+import { fetch } from "@tauri-apps/plugin-http";
+import WebSocket from "@tauri-apps/plugin-websocket";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 
 const state = reactive({
   price: "",
-  priceZS: '',
-  color: '',
-  color2: '',
-  priceDP: '',
-})
-
-var ws;
-
-const headersend = async () => {
-  await ws.send(JSON.stringify({
-    "cmd_id": 22000,
-    "seq_id": 123,
-    "trace": "719292a3a922cfa946a49923c29fb7e5-c-app",
-    "data": {}
-  }));
-}
-
-
-
-
-const initWS = async () => {
-  ws = await WebSocket.connect('wss://quote.tradeswitcher.com/quote-b-ws-api?token=719292a3a922cfa946a49923c29fb7e5-c-app');
-
-  ws.addListener((msg) => {
-    let data = JSON.parse(msg.data)
-    console.log('Received Message:', data);
-    if (data?.data?.price) {
-      state.priceDP = data?.data?.price
-
-    }
-  });
-
-
-  await ws.send(JSON.stringify({
-    "cmd_id": 22004,
-    "seq_id": 123,
-    "trace": "719292a3a922cfa946a49923c29fb7e5-c-app",
-    "data": {
-      "symbol_list": [
-        {
-          "code": "GOLD",
-        }
-      ]
-    }
-  }));
-  setInterval(() => {
-    headersend()
-  }, 10000);
-}
-
-
-initWS()
+  priceZS: "",
+  color: "",
+  color2: "",
+  priceDP: "",
+});
 
 const getJDPrice = async () => {
-
-
-  const data = await fetch('https://api.jdjygold.com/gw/generic/hj/h5/m/latestPrice', {
-    method: 'GET',
-    timeout: 30,
-  }).then(response => response.json());
-  state.price = data.resultData.datas.price
-
-}
+  const data = await fetch(
+    "https://api.jdjygold.com/gw/generic/hj/h5/m/latestPrice",
+    {
+      method: "GET",
+      timeout: 30,
+    }
+  ).then((response) => response.json());
+  state.price = data.resultData.datas.price;
+};
 const getJDPriceZS = async () => {
-  const data = await fetch('https://api.jdjygold.com/gw2/generic/jrm/h5/m/stdLatestPrice?productSku=1961543816', {
-    method: 'post',
-    data: {
-      reqData: { "productSku": "1961543816" }
-    },
-    timeout: 30,
-  }).then(response => response.json());
-  state.priceZS = data.resultData.datas.price
-
-}
-
-
+  const data = await fetch(
+    "https://api.jdjygold.com/gw2/generic/jrm/h5/m/stdLatestPrice?productSku=1961543816",
+    {
+      method: "post",
+      data: {
+        reqData: { productSku: "1961543816" },
+      },
+      timeout: 30,
+    }
+  ).then((response) => response.json());
+  state.priceZS = data.resultData.datas.price;
+};
 
 const getPrice = async () => {
-  getJDPrice()
-  getJDPriceZS()
-}
-getPrice()
+  getJDPrice();
+  getJDPriceZS();
+};
+getPrice();
 setInterval(() => {
-  getPrice()
+  getPrice();
 }, 3000);
 
-const close = async() => {
+const close = async () => {
   await getCurrentWindow().close();
-}
+};
 </script>
 
 <template>
-
-
-  <div data-tauri-drag-region style="margin-top: 3px;cursor: default;" class="red">
+  <div
+    data-tauri-drag-region
+    style="margin-top: 3px; cursor: default"
+    class="red"
+  >
     民生：{{ state.price }}
-
   </div>
-  <div data-tauri-drag-region style="margin-top: 0px;cursor: default;" class="red">
+  <div
+    data-tauri-drag-region
+    style="margin-top: 0px; cursor: default"
+    class="red"
+  >
     浙商：{{ state.priceZS }}
   </div>
-  <div data-tauri-drag-region style="margin-top: 0px;cursor: default;" class="red">
-    现货：{{ state.priceDP }}
-  </div>
-  <div class="close" @click="close">
-    关闭
-  </div>
+  <div class="close" @click="close">关闭</div>
 </template>
 
 <style>
@@ -121,10 +74,19 @@ const close = async() => {
   margin: 0;
 }
 
-html {
-  background-color: #2f2f2f;
-  padding-left: 10px;
+@media (prefers-color-scheme: dark) {
+  html {
+    background-color: #2f2f2f;
+    padding-left: 10px;
+  }
+}
 
+/* 当系统处于亮色模式（偏好亮色主题）时应用以下样式 */
+@media (prefers-color-scheme: light) {
+  html {
+    background-color: white;
+    padding-left: 10px;
+  }
 }
 
 html :hover .close {
@@ -133,7 +95,6 @@ html :hover .close {
 
 .red {
   color: red;
-
 }
 
 .green {
